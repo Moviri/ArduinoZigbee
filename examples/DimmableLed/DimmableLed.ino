d0f8d0ffac8e5f98f5b4ac4dac50c0f4ecac3f54
Valerio-Colombo <valecolomb96@gmail.com>
2022-01-26 10:58:32 +0100

    Examples refactoring

--- examples/DimmableLed/DimmableLed.ino
+++ examples/DimmableLed/DimmableLed.ino
@@ -12,27 +12,18 @@ void dimLightCB_2(const uint8_t brightness_level) {
 #define EP_2 13
 
 //name, endpoint id, callback
-DimmableLight(light_1, EP_1, dimLightCB_1)
-DimmableLight(light_2, EP_2, dimLightCB_2)
-
-//ZB_DIMMABLE_LIGHT(light_2, EP_2, void [](const uint8_t brightness_level) 
-//  {
-//    analogWrite(LEDG, 255 - brightness_level);
-//  }
-//)
-
-//ZB_DIMMABLE_LIGHT(light_2, EP_2, 
-//  (const uint8_t brightness_level) 
-//  {
-//    analogWrite(LEDG, 255 - brightness_level);
-//  }
-//)
+DimmableLight(light_1, EP_1, dimLightCB_1);
+DimmableLight(light_2, EP_2, dimLightCB_2);
 
 void setup() {
+  /*
+  // If you want to explicity set the TC_KEY uncomment this section
+
   // ZLL Commissioning Trust Centre Link Key
-  #define ZB_ZLL_TC_KEY {}; // insert TC_KEY if known (FF, FF, ...)
+  #define ZB_ZLL_TC_KEY {}; // insert TC_KEY as HEX if known (FF, FF, ...)
   zb_uint8_t zb_zll_tc_key[] = ZB_ZLL_TC_KEY;
   ZIGBEE.setTrustCenterKey(zb_zll_tc_key);
+  */
   ZIGBEE.begin();
 }
 void loop() {
---
+++ examples/TemperatureSensor/TemperatureSensorNordicDK/TemperatureSensor/TemperatureSensor.ino
@@ -0,0 +1,28 @@
+/*
+  Zigbee HA Temperature Sensor example
+
+  This example creates a zigbee device following the HA(Home Automation) profile.
+  It uses a simulated temperature sensor
+
+  The circuit:
+  - nRF52840DK board
+
+  This example code is in the public domain.
+*/
+#include <ArduinoZigbee.h>
+
+float tempSensorCB_1() {
+  // Put sensor read here
+  return float(random(20.0,28.0)); 
+}
+
+#define EP_1 15
+
+TemperatureSensor(temp_1, EP_1, tempSensorCB_1, 2000);
+
+void setup() {
+  ZIGBEE.begin(1 << 11);
+}
+void loop() {
+  ZIGBEE.poll();
+}
